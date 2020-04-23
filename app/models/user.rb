7a0661e8d55a2a606e2e20ed7fd46a2352f3c9f0
Sota <sooota09@gmail.com>
2020-04-24 03:03:47 +0900

    [update]

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,6 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
-  has_many :relationships, dependent: :destroy
 
   # フォロー機能アソシエーション-start-
 
